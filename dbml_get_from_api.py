debf6ed508f0886497020e58cf514631d3a60395
ioatzim <36932773+ioatzim@users.noreply.github.com>
2021-11-17 00:01:17 +0200

    Update dbml_get_from_api.py

--- dbml_get_from_api.py
+++ dbml_get_from_api.py
@@ -7,7 +7,7 @@ import datetime
 Pulls a dbml file from the API. User must manually add the file id, found in the 'response_ids.json' file generated from dbml_post_to_api.py
 '''
 
-url='http://ec2-54-167-67-34.compute-1.amazonaws.com/api/dbmls' 
-id = '6192b1f31c2a512293fea940'
+url='http://ec2-54-167-67-34.compute-1.amazonaws.com/api/dbmls' #url of the API
+id = '6192b1f31c2a512293fea940' #id of the file, taken from 'response_ids.json' file generated from dbml_post_to_api.py
 res = requests.get(f'{url}/{id}')
 dbml_file = json.loads(res.json()['contents'])
